5a2e6a3c17cd56616534f995e380be6e635f5477
Justin Xu <justinxu2014@gmail.com>
2025-03-31 15:04:18 -0400

    fixed an index issue

--- module-3/analysis.py
+++ module-3/analysis.py
@@ -21,16 +21,17 @@ for idx, row in RAW_RECIPES.iterrows():
 temp = []
 
 for idx in range(len(RECIPES["id"])):
-    temp.append([0] * max(ingr_map["id"]))
+    temp.append([0] * max(ingr_map["id"] + 1))
 
 for idx in range(len(temp)): 
     for ingredient_id in json.loads(RECIPES.iloc[idx]["ingredient_ids"]):
         temp[idx][ingredient_id] = 1
 
-col= list(range(0 , max(ingr_map["id"])))
+col= list(range(0 , max(ingr_map["id"] + 1)))
 
 DF = pd.DataFrame.from_records(data=temp, columns=col, index=RECIPES["id"])
 
+# Select target for comparison
 target = 424415 
 target_ = DF.loc[target]
 
